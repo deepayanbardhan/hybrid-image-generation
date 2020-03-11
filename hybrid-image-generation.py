34a4dd0e785d783846fb893bd50efc21cc6b9178
deepayanbardhan <53681666+deepayanbardhan@users.noreply.github.com>
2020-03-11 11:05:55 -0400

    Add files via upload

---
+++ hybrid-image-generation.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+
+def ft(img):
+    d= np.fft.fft2(img)
+        
+    #FOR DISPLAYING THE FREQUENCY ANALYSIS:
+    """mag=(20*np.log(np.abs(ds)))
+    mag=mag/np.max(mag)*255
+    #mag[mag>255]=255
+    mag=np.uint8(mag)
+    
+    cv2.imshow('freq',mag)
+    cv2.waitKey(0)"""
+    return d
+
+def ift(freq_img):
+    f_ishift = np.fft.ifftshift(freq_img)
+    img_back = np.fft.ifft2(f_ishift)
+    img_back = np.uint8(np.abs(img_back))
+    return img_back
+
+def hybrid(img1,img2):
+    fft1=ft(img1)
+    fft2=ft(img2)
+    newfft=(2*fft1+1*fft2)/3;
+    return ift(newfft)
+
+img1 = cv2.imread('D:\\deepayan\\study\\study\\sem 1\\ECE 558\\HW02-Due10-03\\dbardha_hw02\\mac.jpg',0)
+img2 = cv2.imread('D:\\deepayan\\study\\study\\sem 1\\ECE 558\\HW02-Due10-03\\dbardha_hw02\\kfc.jpg',0)
+ans=hybrid(img1,img2)
+#DISPLAY
+cv2.imshow('image1',img1)
+cv2.imshow('image2',img2)
+cv2.imshow('hybrid',ans)
+cv2.waitKey(0)
